348bcbdf750aedb42f2989b571fc75db1dfd448c
Di828 <109897280+Di828@users.noreply.github.com>
2024-05-09 14:18:28 +0300

    Update SqlRequest.sql

--- SQLTest/SqlRequest.sql
+++ SQLTest/SqlRequest.sql
@@ -1,3 +1,3 @@
 SELECT product.name AS product, category.name AS category FROM products
-LEFT JOIN product_category USING (product_id)
-LEFT JOIN category USING (category_id)
+LEFT JOIN product_category on product.id = product_category.id
+LEFT JOIN category on product_category.id = category.id
